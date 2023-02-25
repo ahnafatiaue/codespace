48fb27c54325c97762d280d34962fb6f44ee7e9e
ahnafatiaue <119801220+ahnafatiaue@users.noreply.github.com>
2023-02-25 17:04:41 +0000

    Sat, Feb 25, 2023, 5:04 PM +00:00

--- new.c
+++ new.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    long long n, m, a; // using long long to avoid integer overflow
-    scanf("%lld %lld %lld", &n, &m, &a);
-
-    // calculate the number of flagstones needed for the length and width separately
-    long long length_flagstones = (n + a - 1) / a; // round up using (n + a - 1) / a formula
-    long long width_flagstones = (m + a - 1) / a;
-
-    // calculate the total number of flagstones needed
-    long long total_flagstones = length_flagstones * width_flagstones;
-    printf("%lld\n", total_flagstones);
+   
 
     return 0;
 }
