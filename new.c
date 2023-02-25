a6b850f146db3785ccea8c66f9d7d14ad8ad2bc4
ahnafatiaue <119801220+ahnafatiaue@users.noreply.github.com>
2023-02-25 16:31:16 +0000

    Sat, Feb 25, 2023, 4:31 PM +00:00

---
+++ new.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+
+{
+      int n,m,a,AreaOfTheatre,AreaOfFlagstone;
+      scanf("%d%d%d",&n,&m,&a);
+      AreaOfTheatre = n * m ;
+      AreaOfFlagstone = a*a;
+      
+
+
+
+
