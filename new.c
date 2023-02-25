578de8cb2ce39fb7edde62fcbcbd9851ac4db054
ahnafatiaue <119801220+ahnafatiaue@users.noreply.github.com>
2023-02-25 17:10:53 +0000

    Sat, Feb 25, 2023, 5:10 PM +00:00

--- new.c
+++ new.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main() {
-   
+   int x,total_steps=0,remaining,a;
+   scanf("%d",&x);
+   a = x/5;
+   remaining = x%5;
+
+   total_steps+=a;
+   remaining/=4;
+
+
+
 
     return 0;
 }
