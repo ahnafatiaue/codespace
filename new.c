57cc84764305a7d7566b7c6641c20fdf57a12219
ahnafatiaue <119801220+ahnafatiaue@users.noreply.github.com>
2023-02-25 17:28:47 +0000

    Sat, Feb 25, 2023, 5:28 PM +00:00

--- new.c
+++ new.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
 int main() {
-   int x,total_steps=0,remaining,a,i;
+   int x,total_steps=0,remaining,move,i;
    scanf("%d",&x);
-   
+   for(i=1;i<=5;i++)
+   {
+      move = x/i;
+      remaining=x%i;
+      total_steps+=move;
+
+
+   }
+   printf("%d",total_steps);
 
 
 
 
-
+//gcc new.c -o a; ./a
 
     return 0;
 }
