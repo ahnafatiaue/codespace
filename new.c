b713e7c187881c438c2542bd7fa3189c5fe86af3
ahnafatiaue <119801220+ahnafatiaue@users.noreply.github.com>
2023-02-25 17:44:44 +0000

    Sat, Feb 25, 2023, 5:44 PM +00:00

--- new.c
+++ new.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
 int main() {
-   int a,b,;
+   int a,b,i,count=0;
    scanf("%d%d",&a,&b);
    for(i=a;i<=b;i++)
    {
+    c = i;
+    d = i;
     
+    while(i!=0)
+    {
+        i%10;
+        count++; // count the digits
+    }
    }
 
 
